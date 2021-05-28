d128dfca8059f394c96528f366358f12edb44ad0
Ayush <ayushmdr5@gmail.com>
2021-05-28 11:20:24 +0545

    added upto 5 level

--- js/entities/Koopa.js
+++ js/entities/Koopa.js
@@ -2,7 +2,7 @@ import Entity, { Characteristic } from "../Entity.js";
 import { loadSpriteSheet } from "../loaders/sprite.js";
 import Killable from "../characteristics/Killable.js";
 import KGwalk from "../characteristics/KGwalk.js";
-import Physics from '../characteristics/Physics.js'
+import Physics from "../characteristics/Physics.js";
 
 export function loadKoopa() {
   return loadSpriteSheet("koopa").then(createKoopaFactory);
@@ -11,15 +11,15 @@ export function loadKoopa() {
 const STATE_WALKING = "walking";
 const STATE_HIDING = "hiding";
 const STATE_PANIC = "panic";
-const STATE_DEAD = 'dead'
+const STATE_DEAD = "dead";
 
 class Behavior extends Characteristic {
   constructor() {
     super("behavior");
     this.state = STATE_WALKING;
-    this.hideTime = 0
-    this.hideDuration = 5
-    this.panicSpeed = 1350
+    this.hideTime = 0;
+    this.hideDuration = 7;
+    this.panicSpeed = 350;
   }
   collides(us, them) {
     if (us.killable.dead) {
@@ -27,72 +27,64 @@ class Behavior extends Characteristic {
     }
     if (them.stomper) {
       if (them.vel.y > us.vel.y) {
-        this.handleStomp(us, them)
-      }
-      else if (them.powerup.isPowerup) {
-        us.killable.kill()
+        this.handleStomp(us, them);
+      } else if (them.powerup.isPowerup) {
+        us.killable.kill();
         us.kgwalk.speed = 0;
-        this.state = STATE_DEAD
-        them.sounds.add('powerdown')
+        this.state = STATE_DEAD;
+        them.sounds.add("powerdown");
         them.powerup.isPowerup = false;
         return;
-      }       
-      else {
-        this.handleNudge(us, them)
-        // them.killable.kill();
-      } 
-    }
-  }
-
-  handleNudge(us, them){
-    if (this.state === STATE_WALKING){
-      them.killable.kill()
-    } else if (this.state === STATE_HIDING){
-      this.panic(us, them)
-    } else if (this.state === STATE_PANIC){
-      const travelDir = Math.sign(us.vel.x)
-      const impactDir = Math.sign(us.pos.x - them.pos.x)
-      if (travelDir !== 0 && travelDir !== impactDir){
-        them.killable.kill()
+      } else {
+        this.handleNudge(us, them);
       }
     }
   }
-  handleStomp(us, them){
-    if (this.state == STATE_WALKING){
-      this.hide(us)
-    } else if (this.state === STATE_HIDING){
-      us.vel.x = 100
-      us.kgwalk.speed = 110
-      us.vel.set(200, -200)
 
-    } else if (this.state === STATE_PANIC){
-      this.hide(us)
+  handleNudge(us, them) {
+    if (this.state === STATE_WALKING) {
+      them.killable.kill();
+    } else if (this.state === STATE_HIDING) {
+      this.panic(us, them);
+    } else if (this.state === STATE_PANIC) {
+      them.killable.kill();
+    }
+  }
+  handleStomp(us, them) {
+    if (this.state == STATE_WALKING) {
+      this.hide(us);
+    } else if (this.state === STATE_HIDING) {
+      // us.vel.x = 500
+      us.kgwalk.speed = 210;
+      us.vel.set(200, -200);
+    } else if (this.state === STATE_PANIC) {
+      this.hide(us);
     }
   }
 
-  hide(us){
+  hide(us) {
     us.vel.x = 0;
-    us.kgwalk.speed = 0
-    this.state = STATE_HIDING
+    us.kgwalk.speed = 0;
+    this.state = STATE_HIDING;
   }
 
-  unhide(us){
+  unhide(us) {
     us.kgwalk.speed = 40;
-    this.hideTime = 0
-    this.state = STATE_WALKING
+    this.hideTime = 0;
+    this.state = STATE_WALKING;
   }
 
-  panic(us, them){
-    us.kgwalk.speed = this.panicSpeed * Math.sign(them.vel.x)
-    this.state = STATE_PANIC
+  panic(us, them) {
+    us.kgwalk.speed = this.panicSpeed * Math.sign(them.vel.x);
+    this.state = STATE_PANIC;
   }
 
-  update(us, gameContext){
-    const deltaTime = gameContext.deltaTime
-    if(this.state === STATE_HIDING){
-      this.hideTime += deltaTime
-      if(this.hideTime > this.hideDuration){
-        this.unhide(us)
+  update(us, gameContext) {
+    const deltaTime = gameContext.deltaTime;
+    if (this.state === STATE_HIDING) {
+      this.hideTime += deltaTime;
+      if (this.hideTime > this.hideDuration) {
+        this.unhide(us);
       }
     }
   }
@@ -100,18 +92,19 @@ class Behavior extends Characteristic {
 
 function createKoopaFactory(sprite) {
   const walkAnimation = sprite.animations.get("walk");
-  const wakeAnimation = sprite.animations.get('wake')
+  const wakeAnimation = sprite.animations.get("wake");
 
   function routeAnimation(koopa) {
-    
-    if(koopa.behavior.state === STATE_HIDING  ){
-        if(koopa.behavior.hideTime > 3){
-          return wakeAnimation(koopa.behavior.hideTime)
-        }
-      return  'hiding'
-    }
-    else if (koopa.behavior.state === STATE_PANIC || koopa.behavior.state === STATE_DEAD){
-      return 'hiding'
+    if (koopa.behavior.state === STATE_HIDING) {
+      if (koopa.behavior.hideTime > 4) {
+        return wakeAnimation(koopa.behavior.hideTime);
+      }
+      return "hiding";
+    } else if (
+      koopa.behavior.state === STATE_PANIC ||
+      koopa.behavior.state === STATE_DEAD
+    ) {
+      return "hiding";
     }
     return walkAnimation(koopa.lifeTime);
   }
@@ -124,7 +117,7 @@ function createKoopaFactory(sprite) {
     const koopa = new Entity();
     koopa.size.set(16, 24);
 
-    koopa.addCharacteristic(new Physics())
+    koopa.addCharacteristic(new Physics());
     koopa.addCharacteristic(new KGwalk());
     koopa.addCharacteristic(new Behavior());
     koopa.addCharacteristic(new Killable());
